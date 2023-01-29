7ba8967d221c6934f3e04a78721b3fa8ee5e82a3
claudiaioanacoste <ccsd1930@scs.ubbcluj.ro>
2023-01-29 12:20:57 +0200

    add clear dataset part and seprate normalizing process from splitting process

--- data_pre.py
+++ data_pre.py
@@ -10,6 +10,7 @@ import pandas as pd
 
 
 # 1. Load original dataset
+from IPython.core.display import display
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 
@@ -19,7 +20,26 @@ df = pd.read_csv('data/dataset_original.csv')
 # print(df.to_string())
 
 # 2. Clear NaN or None values from the dataset
-# 2.1 TODO clear dataset from - or -- values, replace them with the value e.g. None for categorical columns
+# 2.1 Clear dataset from - or -- values, replace them with the value e.g. None for categorical columns
+df['CHARSET'] = df['CHARSET'].apply(lambda x: x.lower() if(x != 'None') else x)
+# df["SERVER"] = df['SERVER'].replace('nan', 'None')
+# print(df['SERVER'].unique())
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace('United Kingdom', 'GB')
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace('UK', 'GB')
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace("[u'GB'; u'UK']", 'GB')
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace('ru', 'RU')
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace('se', 'SE')
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace('us', 'US')
+df["WHOIS_COUNTRY"] = df['WHOIS_COUNTRY'].replace('Cyprus', 'CY')
+# print(df['WHOIS_COUNTRY'].unique())
+df["WHOIS_STATEPRO"] = df['WHOIS_STATEPRO'].replace('NONE', 'None')
+df["WHOIS_STATEPRO"] = df['WHOIS_STATEPRO'].replace('-', 'None')
+df["WHOIS_STATEPRO"] = df['WHOIS_STATEPRO'].replace('--', 'None')
+df["WHOIS_STATEPRO"] = df['WHOIS_STATEPRO'].replace('Other', 'None')
+df["WHOIS_STATEPRO"] = df['WHOIS_STATEPRO'].replace('NOT APPLICABLE', 'None')
+df["WHOIS_STATEPRO"] = df['WHOIS_STATEPRO'].replace('Not Applicable', 'None')
+df['WHOIS_STATEPRO'] = df['WHOIS_STATEPRO'].apply(lambda x: x.upper() if(x != 'None') else x)
+# print(df['WHOIS_STATEPRO'].unique())
 
 # 2.2 Clear DATE columns
 df.loc[df["WHOIS_REGDATE"] == "None", "WHOIS_REGDATE"] = "01/01/1970 00:00"
@@ -32,11 +52,16 @@ df.loc[df["WHOIS_UPDATED_DATE"] == "b", "WHOIS_UPDATED_DATE"] = "01/01/1970 00:0
 df.loc[df["WHOIS_UPDATED_DATE"] == "0", "WHOIS_UPDATED_DATE"] = "01/01/1970 00:00"
 df['WHOIS_UPDATED_DATE'] = df['WHOIS_UPDATED_DATE'].apply(lambda x: convert_string_to_datetime(x))
 
-# 2.3 Clear NaN values from numerical columns and replace with 0.0
+# 2.3 Clear NaN values from numerical columns and replace with 0.0 TODO
+# Numerical columns: URL_LENGTH, NUMBER_SPECIAL_CHARACTERS, CONTENT_LENGTH, TCP_CONVERSATION_EXCHANGE,
+# DIST_REMOTE_TCP_PORT, REMOTE_IPS, APP_BYTES, SOURCE_APP_PACKETS, REMOTE_APP_PACKETS, SOURCE_APP_BYTES,
+# REMOTE_APP_BYTES, APP_PACKETS, DNS_QUERY_TIMES, Type
+df = df.fillna(0)
+# print(df.to_string())
+
 
 # 3. Categorical features in two formats
 # Categorical features: CHARSET, SERVER, WHOIS_COUNTRY, WHOIS_STATEPRO,
-# TODO clear dataset from - or -- values, replace them with the value e.g. None for categorical columns
 # 3.1. Dates to timespamps: WHOIS_REGDATE,WHOIS_UPDATED_DATE => these are not categorical features: why?
 
 df['WHOIS_REG_TIMESPANP'] = df['WHOIS_REGDATE'].apply(lambda x: convert_datetime_to_timestamp(x))
@@ -100,11 +125,12 @@ df = compute_weight_of_evidence_algorithm_for_column("SERVER", "SERVER_2", "Type
 df = compute_weight_of_evidence_algorithm_for_column("WHOIS_COUNTRY", "WHOIS_COUNTRY_2", "Type", df)
 df = compute_weight_of_evidence_algorithm_for_column("WHOIS_STATEPRO", "WHOIS_STATEPRO_2", "Type", df)
 
-# DROP categorical columns and URL column (which identifies each URL through an ID)
-df.drop(columns=['URL', 'CHARSET', 'SERVER', 'WHOIS_COUNTRY', 'WHOIS_STATEPRO'], inplace=True, axis = 1)
-print(df.to_string())
+# DROP categorical columns and URL column (which identifies each URL through an ID) and datetime columns
+df.drop(columns=['URL', 'CHARSET', 'SERVER', 'WHOIS_COUNTRY', 'WHOIS_STATEPRO',
+                 'WHOIS_UPDATED_DATE', 'WHOIS_REGDATE'], inplace=True, axis = 1)
+# print(df.to_string())
 
-# 4. Normalize all data with MinMax Scaler and StandardScaler
+# 4.1 Normalize all data with MinMax Scaler and StandardScaler
 
 def normalize_data(df, normalize = True, scaler = 'min-max'):
     """
@@ -116,11 +142,11 @@ def normalize_data(df, normalize = True, scaler = 'min-max'):
     :return: scaled_df: dataframe with normalized dataset (if normalize_data is True, otherwise is the initial dataset)
     """
     # # Split df into X and y
-    y = df['label'].copy()
-    X = df.drop('label', axis=1).copy()
+    y = df['Type'].copy()
+    X = df.drop('Type', axis=1).copy()
 
     # Normalize data along the columns
-    if (normalize_data == True):
+    if (normalize == True):
         if scaler == 'min-max':
             # Normalize with MinMaxScaler
             scaler = MinMaxScaler()
@@ -135,6 +161,12 @@ def normalize_data(df, normalize = True, scaler = 'min-max'):
         scaled_df = X.copy(deep = True)
     return y, scaled_df
 
+# 4.2 Export normalized dataframe to CSV file
+def export_dataframe_to_file(y, X, file):
+    result_df = pd.concat([X, y], axis=1, join='inner')
+    with open(file,'w+'):
+        result_df.to_csv(file, index = False, encoding = 'utf-8')
+
 # 5. Randomly split data into training and testing
 def split_data_in_testing_training(y, X, stratify = False, train_size = 0.8):
     # Train-test split
@@ -145,6 +177,13 @@ def split_data_in_testing_training(y, X, stratify = False, train_size = 0.8):
 
     return X_train, X_test, y_train, y_test
 
+
+
 y, X = normalize_data(df, normalize=True, scaler='min-max')
-X_train, X_test, y_train, y_test = split_data_in_testing_training(y, X, stratify=True, train_size=0.8)
+export_dataframe_to_file(y, X, 'data/dataset_norm_min_max.csv')
+y, X = normalize_data(df, normalize=True, scaler='standard')
+export_dataframe_to_file(y, X, 'data/dataset_norm_standard.csv')
+y, X = normalize_data(df, normalize=False, scaler='standard')
+export_dataframe_to_file(y, X, 'data/dataset_not_norm.csv')
+# X_train, X_test, y_train, y_test = split_data_in_testing_training(y, X, stratify=True, train_size=0.8)
 
